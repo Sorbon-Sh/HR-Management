92d9456090f708c28c4ec8ab874a7e972d1b20bf
Sorbon <mark.jonsan@bk.ru>
2025-05-26 11:56:44 +0500

    build is chaned

--- src/shared/hooks/useReduxTypedHooks.ts
+++ src/shared/hooks/useReduxTypedHooks.ts
@@ -4,4 +4,4 @@ import type { AppDispatch, RootState } from '../redux/store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppSelector = useSelector.withTypes<RootState>() 
