a3b56fead019cbbf18d6293b01f570d558a1ce5b
Sorbon <mark.jonsan@bk.ru>
2025-10-25 17:09:17 +0500

    Ошибки авторизации и регистраыии обработаны на 90%

--- src/components/pages/AuthPage.tsx
+++ src/components/pages/AuthPage.tsx
@@ -8,11 +8,20 @@ import { toast, ToastContainer } from "react-toastify";
 
 export default function AuthPage() {
 
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   const navigation = useNavigate()
-  const {register,handleSubmit,reset} = useForm<InputsAuth>()
+  const {register,handleSubmit,reset,formState: { errors }} = useForm<InputsAuth>()
+
+// * Блок обработки ошибок формы
+  const onError = () => {
+  if (errors.password?.message) {
+    toast.error(errors.password.message);
+  }
+};
+
   const submit: SubmitHandler<InputsAuth> = async  (formData) => {
 
+// * Блок Входа в систему
 if(isLogin){
    const toastLoading = toast.loading("Вход в систему...")
     const {data, error} = await supabase.auth.signInWithPassword({
@@ -20,7 +29,7 @@ if(isLogin){
     password: String(formData.password),
   })
 
-  
+  // * Блок Обработка ошибок входа
   if(error){ 
       toast.update(toastLoading, 
 {render: "Не правильный логин или пароль!",
@@ -32,34 +41,58 @@ if(isLogin){
   }
 
   if(data) navigation("/")
-  console.log("Login data: ", data)
-
 reset()
 }
 
-
+// * Блок Регистрации пользователя
   if(!isLogin){
+  const toastLoading = toast.loading("Регистрация пользователя...")
+
   const { data, error } = await supabase.auth.signUp({
   email: formData.email,
   password: String(formData.password),
 })
 
-if (error) throw new Error(error.message);
+// * Блок создания профиля пользователя
 // * Создаем доп данные пользователя в Supabase (Cоздаём профиль) с методом Profiles
 if (data.user) {
+
   const {error: profileError } = await supabase.from("profiles").insert({
     id: data.user.id,
-    full_name: formData.fullName,
+    fullname: formData.fullName,
     role: "user", // можно задать по умолчанию
   });
 
   if (profileError) throw new Error(profileError.message);
 }
 
-navigation("/")
+// * Блок Обработка ошибок регистрации
+if(error){ 
+  
+    if(error.status === 422) {
+         toast.update(toastLoading, 
+{render: "Такой пользователь уже существует!",
+   type: "error", 
+   isLoading: false,
+   autoClose: 3000})
+  }
+
+      if(error.status === 0) {
+         toast.update(toastLoading, 
+{render: "Нет интернета!",
+   type: "error", 
+   isLoading: false,
+   autoClose: 3000})
+  }
+
+  throw new Error(`${error.status}`)
+}
+
+if(data.user) navigation("/")
 }
   }
 
+ 
 
   return (
     <div className="flex h-screen">
@@ -79,11 +112,14 @@ navigation("/")
             {isLogin ? "Вход в систему" : "Регистрация"}
           </h1>
 
-          <form onSubmit={handleSubmit(submit)} className="space-y-4">
+          <form onSubmit={handleSubmit(submit, onError)} className="space-y-4">
             {!isLogin && (
               <input
                 type="text"
-                {...register("fullName")}
+                {...register("fullName", {
+                  required: true,
+                  setValueAs: (value) => value.trim(),
+                })}
                 placeholder="ФИО"
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
@@ -91,14 +127,28 @@ navigation("/")
 
             <input
               type="email"
-              {...register("email", { required: true })}
+              {...register("email", {
+                 required: true,
+                 setValueAs: (value) => value.trim() })}
               placeholder="E-mail"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
 
             <input
               type="password"
-              {...register("password", { required: true })}
+             {...register("password", {
+    setValueAs: (value) => value.trim(),
+    required: true,
+    minLength: {
+      value: 8,
+      message: "Минимальная длина пароля — 8 символов",
+    },
+    pattern: {
+      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/,
+      message:
+        "Пароль должен содержать хотя бы одну цифру, одну заглавную и одну строчную букву",
+    },
+  })}
               placeholder="Пароль"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
